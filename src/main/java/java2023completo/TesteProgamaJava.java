0aafd0c3f7c9823f30a9b48393ea178a3ee1f151
AngeloSanthiago <angelosanthiago@gmail.com>
2023-12-26 21:19:09 -0300

    Video-16 completo

--- src/main/java/java2023completo/TesteProgamaJava.java
+++ src/main/java/java2023completo/TesteProgamaJava.java
@@ -3,6 +3,7 @@ package java2023completo;
 public class TesteProgamaJava {
 	public static void main(String[] args) {
 		System.out.println("Teste de execução de classe");
+		System.out.println("Teste de execução de segunda linha");
 	}
 
 }
