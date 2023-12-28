ad538388e4492213cd57ec47c1191abe7f988533
AngeloSanthiago <angelosanthiago@gmail.com>
2023-12-27 21:28:54 -0300

    Video23 - resolução pessoal e em conjunto do desafio

--- src/main/java/fundamentos/Temperatura.java
+++ src/main/java/fundamentos/Temperatura.java
@@ -5,5 +5,20 @@ public class Temperatura {
 //(ºF - 32) X 5/9 = ºC
 		
 		
+		
+//		resolucao_pessoal();
+		
+	}
+
+	private static void resolucao_pessoal() {
+		double tempF = 100.75;
+		double tempC;
+		final int const1 = 32;
+		final double const2 = 5.0/9;
+		
+		System.out.println("Promagrama para conversão de Fahrenheit para Celsius");
+		System.out.println("Temperatura em Fahrenheit para conversão é de: " + tempF + "ºF");
+		tempC = (tempF - const1) * const2;
+		System.out.println("Temperatura em Celsius é de " + tempC + "ºC");
 	}
 }
