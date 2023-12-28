824d094ca3eba93335ea5f502edf26bafdc52ecd
AngeloSanthiago <angelosanthiago@gmail.com>
2023-12-28 19:53:23 -0300

    Video20 - conclusão

--- src/main/java/video20/ExpressoesMatematicas.java
+++ src/main/java/video20/ExpressoesMatematicas.java
@@ -4,6 +4,10 @@ public class ExpressoesMatematicas {
 	public static void main(String[] args) {
 		System.out.println(3 + 2 * 4);
 		System.out.println((3 + 2) * 4);
+		System.out.println(60 / (3 + 2) * 4);
+		System.out.println(60 / ((3 + 2) * 4));
+		System.out.println("Mod: " + 14 % 3);
+		System.out.println("Mod: " + 19 % 5);
 	}
 
 }
