338ad589f15bae1686b0a077a7bbd646ef57ff2e
samue <samuel1990.sf.sf@gmail.com>
2024-01-20 21:05:37 -0300

    video 47 completo

--- src/main/java/fundamentos/operadores/Logicos.java
+++ src/main/java/fundamentos/operadores/Logicos.java
@@ -14,13 +14,28 @@ public class Logicos {
 		System.out.println(!condicao1); // not V = F
 		System.out.println(!condicao2); // not F = V
 		
-		System.out.println("Tabela verdade E");
+
+		System.out.println("Tabela verdade E (AND)");
 		System.out.println(true && true);
 		System.out.println(true && false);
 		System.out.println(false && true);
 		System.out.println(false && false);
 		
+		System.out.println("\nTabela verdade OU (OR)");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
 		
+		System.out.println("\nTabela verdade OU Exclusivo (XOR)");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		System.out.println("\nTabela verdade NOT ");
+		System.out.println(!true);
+		System.out.println(!false);
 	}
 
 }
